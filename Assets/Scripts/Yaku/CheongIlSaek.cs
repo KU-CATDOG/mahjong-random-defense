c96a06014fd248ab39bb0536f1569bfb4a39f4ab
devingryu <devingryu@korea.ac.kr>
2022-07-06 18:27:04 +0900

    CheongIlSaek Fix

--- Assets/Scripts/Yaku/CheongIlSaek.cs
+++ Assets/Scripts/Yaku/CheongIlSaek.cs
@@ -7,10 +7,10 @@ namespace MRD
         public string TargetYakuName => "CheongIlSaek";
 
         public bool CheckCondition(YakuHolderInfo holder)
-        {   //청일색: 몸통 4개와 머리 1개 모두 동일 종류 
-            HaiType cmpbase = holder.MentsuInfos[0].Hais[0].HaiType; // 비교 기준으로 첫 멘쯔의 모양 설정
-            return holder.MentsuInfos.Count(x => (x is KoutsuInfo or KantsuInfo) && x.Hais[0].Spec.HaiType == cmpbase) == 4 && // 몸통 4개
-                   holder.MentsuInfos.Count(x => (x is ToitsuInfo) && x.Hais[0].Spec.HaiType == cmpbase) == 1; //머리 하나
+        {   //청일색: 수패가 모두 같은 모양
+            HaiType cmpbase = holder.MentsuInfos[0].Hais[0].Spec.HaiType; // 비교 기준으로 첫 멘쯔의 모양 설정
+            if (cmpbase is not HaiType.Wan or HaiType.Pin or HaiType.Sou) return false; // 수패가 아니면 false 리턴
+            return holder.MentsuInfos.All(x => (x is KoutsuInfo or KantsuInfo) && x.Hais[0].Spec.HaiType == cmpbase);// 모두 비교대상과 같은지 체크
         }
     }
 }
