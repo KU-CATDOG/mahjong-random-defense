e42c22e0253b9ef6e9b7268c548d2d8b32c6051b
Yoon <cse21jh@korea.ac.kr>
2022-07-07 05:08:08 +0900

    JunJJang

---
+++ Assets/Scripts/Yaku/JunJJang.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+
+namespace MRD
+{
+    public class JunJJangChecker : IYakuConditionChecker
+    {
+        public string TargetYakuName => "JunJJang";
+
+        public bool CheckCondition(YakuHolderInfo holder)
+        {
+            foreach(var i in holder.MentsuInfos.Select(x => x.Hais)) //foreach head & body
+            {
+                if(i.All(x => !x.Spec.IsRoutou)) return false; //no 1, 9
+            }
+            return true;
+        }
+    }
+
+}
