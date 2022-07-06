ce374b2206faf1aa9567068ee29d258d34da5111
devingryu <devingryu@korea.ac.kr>
2022-07-06 19:02:56 +0900

    HonNoDu, HonIlSaek

---
+++ Assets/Scripts/Yaku/HonIlSaek.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+
+namespace MRD
+{
+    public class HonIlSaekChecker : IYakuConditionChecker
+    {
+        public string TargetYakuName => "HonIlSaek";
+
+        public bool CheckCondition(YakuHolderInfo holder)
+        {   //혼일색: 수패와 자패로 이루어지되, 모든 수패가 모양이 같아야 함
+            HaiType? haiType = null; // 첫번째 수패 이전에는 null
+            foreach (var it in holder.MentsuInfos)
+            {
+                if (!it.Hais[0].Spec.IsJi) { // 수패면
+                    if(haiType == null) // 수패 최초 등장 시 기준 타입으로 지정
+                        haiType = it.Hais[0].Spec.HaiType;
+                    else
+                        if(haiType != it.Hais[0].Spec.HaiType) // 두번째부터는 다르면 false 리턴
+                            return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/Yaku/HonNoDu.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+
+namespace MRD
+{
+    public class HonNoDuChecker : IYakuConditionChecker
+    {
+        public string TargetYakuName => "HonNoDu";
+
+        public bool CheckCondition(YakuHolderInfo holder)
+        {   // 혼노두: 모든 패가 노두패(1 혹은 9)와 자패만으로만 이루어짐
+            return holder.MentsuInfos.All(x => x.Hais.All(y => y.Spec.IsYaochu ) );// 모든 패가 조건을 만족하는지 확인
+        }
+    }
+}
