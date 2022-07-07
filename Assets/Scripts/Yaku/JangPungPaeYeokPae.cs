f2e401c90704843f0a3050b79dc59d00e599d2bf
kanghee02 <rkdgml0820@naver.com>
2022-07-07 20:14:30 +0900

    JangPungPaeYeokPae

---
+++ Assets/Scripts/Yaku/JangPungPaeYeokPae.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace MRD
+{
+    public class JangPungPaeYeokPaeChecker : IYakuConditionChecker
+    {
+        public string TargetYakuName => "JangPungPaeYeokPae";
+
+        public bool CheckCondition(YakuHolderInfo holder)
+        {   //장풍패 역패 : 몸통 중 하나가 장풍패 커쯔
+            return holder.MentsuInfos.Where(x => x is KoutsuInfo or KantsuInfo).
+                Any(x => x.Hais[0].Spec.HaiType == HaiType.Kaze &&
+                x.Hais[0].Spec.Number == RoundManager.Inst.round.wind);
+        }
+    }
+}
+
