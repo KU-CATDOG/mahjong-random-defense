53c673250106db97da4df1ed0ea3960230714c59
MyBug18 <mskimkim0318@gmail.com>
2022-07-08 11:33:38 +0900

    Added basic stat to tower

--- Assets/Scripts/Grid/Tower.cs
+++ Assets/Scripts/Grid/Tower.cs
@@ -9,6 +9,20 @@ namespace MRD
 
         public XY Coordinate => Pair.Coordinate;
 
+        /// <summary>
+        /// 모든 타워가 공통으로 가지는 기본 공격력
+        /// </summary>
+        public float BaseAttack => TowerInfo.Hais.Count * 10;
+
+        /// <summary>
+        /// 모든 타워가 공통으로 가지는 기본 공격 속도 (1초에 이만큼 때림)
+        /// </summary>
+        public float BaseAttackSpeed => 1f;
+
+        public float BaseCritChance => 0.05f;
+
+        public float BaseCritMultiplier => 2;
+
         public void Init(GridCell gridCellInstance, XY coord)
         {
             Pair = gridCellInstance;
