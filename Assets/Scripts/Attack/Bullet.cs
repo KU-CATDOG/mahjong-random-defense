22ae69ee4b612fd902c79e15ce8f7484852b30d7
devingryu <devingryu@korea.ac.kr>
2022-07-08 17:56:15 +0900

    Bullet base created

--- Assets/Scripts/Attack/Bullet.cs
+++ Assets/Scripts/Attack/Bullet.cs
@@ -6,9 +6,36 @@ namespace MRD
 {
     public class Bullet : Attack
     {
+        private Vector3 _startLocation;
+        private float _speed;
+        public Vector3 startLocation
+        {
+            get => _startLocation;
+            set
+            {
+                _startLocation = value;
+                this.transform.position = _startLocation;
+            }
+        }
+        public int baseDamage;
+        
+        // Readonly property, should be set with setDirection().
+        public Vector3 direction{ get; private set; }
+        public float speed { get; private set; }
+
+        public void setDirection(Vector3 direction, float speed){
+            this.direction = direction;
+            this.speed = speed;
+
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            
+            if(collision.gameObject.tag == "Enemy"){
+                collision.gameObject.GetComponent<EnemyController>().health -= baseDamage;
+                Destroy(this.gameObject);
+            }
         }
     }
 }
