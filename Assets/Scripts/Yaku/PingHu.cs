96ce26dda815da17b3b6d53faf5527c50db987d2
Yoon <cse21jh@korea.ac.kr>
2022-07-09 03:54:41 +0900

    PingHu

---
+++ Assets/Scripts/Yaku/PingHu.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+
+namespace MRD
+{
+    public class PingHuChecker : IYakuConditionChecker
+    {
+        public string TargetYakuName => "PingHu";
+
+        public bool CheckCondition(YakuHolderInfo holder)
+        {
+            foreach(var m in holder.MentsuInfos)
+            {
+                if (m is ShuntsuInfo) continue;
+                else if (m is ToitsuInfo)
+                {
+                    if (m.Hais[0].Spec.Number == RoundManager.Inst.round.wind) return false;
+                    else continue;
+                }
+                else return false;
+            }
+
+            return true;
+
+            //return holder.MentsuInfos.All(x => x is ShuntsuInfo);
+        }
+    }
+
+}
