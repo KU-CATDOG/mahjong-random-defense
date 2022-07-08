a853f477a33a6ff0ab6512a70f19eb501404cddd
RyuJiSeok <roadmageb@naver.com>
2022-07-08 10:42:29 +0900

    Add bullet

---
+++ Assets/Scripts/Attack/Bullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MRD
+{
+    public class Bullet : Attack
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            
+        }
+    }
+}
