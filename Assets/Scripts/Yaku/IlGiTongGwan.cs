efd77e89ff610765c8c2483abd92b5e048866468
Yoon <cse21jh@korea.ac.kr>
2022-07-09 11:44:20 +0900

    fixed IlGiTongGwan

--- Assets/Scripts/Yaku/IlGiTongGwan.cs
+++ Assets/Scripts/Yaku/IlGiTongGwan.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using UnityEngine;
 
 namespace MRD
 {
@@ -10,21 +11,26 @@ namespace MRD
         {
             int hnum = 0;
             bool[] check = new bool[9];
+            HaiType htype = holder.MentsuInfos[0].Hais[0].Spec.HaiType;
 
             for (int i = 0; i < 9; i++) check[i] = false;
             
-            if (holder.MentsuInfos.Select(x => x.Hais).All(x => x is ShuntsuInfo))
+            if (holder.MentsuInfos.All(x => x is ShuntsuInfo))
             {
                 foreach(var p in holder.MentsuInfos.SelectMany(x => x.Hais))
                 {
                     hnum = p.Spec.Number;
 
+                    if (htype != p.Spec.HaiType) return false;
+
                     if (!check[hnum - 1]) check[hnum - 1] = true; //check 1~9
+                    htype = p.Spec.HaiType;
                 }
 
                 foreach (bool t in check)
                 {
-                    if (!t) return false;
+                    if (!t)
+                        return false;
                 } 
             }
             else return false; //not Shuntsu
