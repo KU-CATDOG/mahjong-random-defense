5ddf24d6cca382502ece44e08ce9d996c2b69a9d
WonKangHee <rkdgml0820@korea.ac.kr>
2022-07-09 19:11:18 +0900

    Added TowerImage

--- Assets/Scripts/Grid/Tower.cs
+++ Assets/Scripts/Grid/Tower.cs
@@ -39,6 +39,48 @@ namespace MRD
             Pair.Init(this, coord);
         }
 
+        [SerializeField]
+        private Sprite[] SingleTowerImages;
+        [SerializeField]
+        private Sprite[] NodeTowerImages;
+        [SerializeField]
+        private Sprite[] TripleTowerImages;
+        [SerializeField]
+        private Sprite[] CompleteTowerImages;
+
+
+        public void ApplyTowerImage()
+        {
+            GameObject TowerImage = new GameObject("TowerImage");
+            TowerImage.transform.parent = transform;
+            TowerImage.transform.localPosition = Vector3.zero;
+            TowerImage.AddComponent<SpriteRenderer>();
+            SpriteRenderer TowerImageSpriteRender = TowerImage.GetComponent<SpriteRenderer>();
+            TowerImageSpriteRender.sortingLayerName = "BattleField";
+            TowerImageSpriteRender.sortingOrder = transform.GetComponent<SpriteRenderer>().sortingOrder + 1;
+
+            Sprite SettingSprite;
+            if (TowerInfo is SingleHaiInfo)
+            {
+                SettingSprite = SingleTowerImages[0];           //종류에 맞는 Sprite 출력하게끔 수정해야 함(중장패인지, 노두패인지 등)
+            }
+            else if (TowerInfo is ToitsuInfo or ShuntsuInfo or KoutsuInfo or KantsuInfo)
+            {
+                SettingSprite = NodeTowerImages[0];
+            }
+            else if (TowerInfo is TripleTowerInfo)
+            {
+                SettingSprite = TripleTowerImages[0];
+            }
+            else
+            {
+                SettingSprite = CompleteTowerImages[0];
+            }
+
+            TowerImageSpriteRender.sprite = SettingSprite;
+        }
+
+
         IEnumerator ShootBullet()
         {
             //총알 생성
