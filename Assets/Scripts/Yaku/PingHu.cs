8e0a2cf310725c66253ed8db13ad37b26530d8aa
Yoon <cse21jh@korea.ac.kr>
2022-07-09 11:27:36 +0900

    fixed PingHu

--- Assets/Scripts/Yaku/PingHu.cs
+++ Assets/Scripts/Yaku/PingHu.cs
@@ -10,18 +10,20 @@ namespace MRD
         {
             foreach(var m in holder.MentsuInfos)
             {
-                if (m is ShuntsuInfo) continue;
-                else if (m is ToitsuInfo)
+                if (!m.IsMenzen) return false;
+                else
                 {
-                    if (m.Hais[0].Spec.Number == RoundManager.Inst.round.wind) return false;
-                    else continue;
+                    if (m is ShuntsuInfo) continue;
+                    else if (m is ToitsuInfo)
+                    {
+                        if (m.Hais[0].Spec.Number == RoundManager.Inst.round.wind || m.Hais[0].Spec.HaiType == HaiType.Sangen) return false;
+                        else continue;
+                    }
+                    else return false;
                 }
-                else return false;
             }
 
             return true;
-
-            //return holder.MentsuInfos.All(x => x is ShuntsuInfo);
         }
     }
 
