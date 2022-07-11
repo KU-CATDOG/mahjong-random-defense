42083e11b3ebb9d1cb261841d06d6b74f97c5a6f
MyBug18 <mskimkim0318@gmail.com>
2022-07-11 10:40:56 +0900

    fixed naming

--- Assets/Scripts/Grid/Tower.cs
+++ Assets/Scripts/Grid/Tower.cs
@@ -7,19 +7,14 @@ namespace MRD
 {
     public class Tower : MonoBehaviour
     {
-        private CircleCollider2D EnemyDetector;
-        private List<GameObject> EnemyInRange = new List<GameObject>();
+        private CircleCollider2D enemyDetector;
+        private List<GameObject> enemyInRange = new List<GameObject>();
         private bool onCoolDown;
-
-        private void Start()
-        {
-            StartCoroutine("ShootBullet", 1);
-        }
 
         public TowerInfo TowerInfo { get; private set; }
         public GridCell Pair { get; private set; }
 
-        public RoundManager RoundManager;
+        public RoundManager roundManager;
 
         public GameObject bullet;
         public XY Coordinate => Pair.Coordinate;
@@ -31,69 +26,75 @@ namespace MRD
             Pair = gridCellInstance;
             Pair.Init(this, coord);
 
-            EnemyDetector = GetComponent<CircleCollider2D>();
+            enemyDetector = GetComponent<CircleCollider2D>();
 
             TowerStat = new TowerStat(this);
         }
 
         [SerializeField]
-        private Sprite[] SingleTowerImages;
+        private Sprite[] singleTowerImages;
         [SerializeField]
-        private Sprite[] NodeTowerImages;
+        private Sprite[] nodeTowerImages;
         [SerializeField]
-        private Sprite[] TripleTowerImages;
+        private Sprite[] tripleTowerImages;
         [SerializeField]
-        private Sprite[] CompleteTowerImages;
+        private Sprite[] completeTowerImages;
 
 
         public void ApplyTowerImage()
         {
-            var TowerImage = new GameObject("TowerImage");
-            TowerImage.transform.parent = transform;
-            TowerImage.transform.localPosition = Vector3.zero;
-            TowerImage.AddComponent<SpriteRenderer>();
-            var TowerImageSpriteRender = TowerImage.GetComponent<SpriteRenderer>();
-            TowerImageSpriteRender.sortingLayerName = "BattleField";
-            TowerImageSpriteRender.sortingOrder = transform.GetComponent<SpriteRenderer>().sortingOrder + 1;
+            var towerImage = new GameObject("TowerImage")
+            {
+                transform =
+                {
+                    parent = transform,
+                    localPosition = Vector3.zero,
+                },
+            };
 
-            Sprite SettingSprite;
+            towerImage.AddComponent<SpriteRenderer>();
+            var towerImageSpriteRender = towerImage.GetComponent<SpriteRenderer>();
+            towerImageSpriteRender.sortingLayerName = "BattleField";
+            towerImageSpriteRender.sortingOrder = transform.GetComponent<SpriteRenderer>().sortingOrder + 1;
+
+            Sprite settingSprite;
             if (TowerInfo is SingleHaiInfo)
             {
-                SettingSprite = SingleTowerImages[0];           //종류에 맞는 Sprite 출력하게끔 수정해야 함(중장패인지, 노두패인지 등)
+                settingSprite = singleTowerImages[0];           //종류에 맞는 Sprite 출력하게끔 수정해야 함(중장패인지, 노두패인지 등)
             }
             else if (TowerInfo is ToitsuInfo or ShuntsuInfo or KoutsuInfo or KantsuInfo)
             {
-                SettingSprite = NodeTowerImages[0];
+                settingSprite = nodeTowerImages[0];
             }
             else if (TowerInfo is TripleTowerInfo)
             {
-                SettingSprite = TripleTowerImages[0];
+                settingSprite = tripleTowerImages[0];
             }
             else
             {
-                SettingSprite = CompleteTowerImages[0];
+                settingSprite = completeTowerImages[0];
             }
 
-            TowerImageSpriteRender.sprite = SettingSprite;
+            towerImageSpriteRender.sprite = settingSprite;
         }
 
         private void FixedUpdate()
         {
-            if (onCoolDown || EnemyInRange.Count <= 0) return;
+            if (onCoolDown || enemyInRange.Count <= 0) return;
 
             onCoolDown = true;
 
             var pos = transform.position;
-            float minDistance = (pos - EnemyInRange[0].transform.position).sqrMagnitude;
-            var proxTeki = EnemyInRange[0];
+            float minDistance = (pos - enemyInRange[0].transform.position).sqrMagnitude;
+            var proxTeki = enemyInRange[0];
 
-            for (int i = 1; i < EnemyInRange.Count; i++)
+            for (int i = 1; i < enemyInRange.Count; i++)
             {
-                var sqrMag = (pos - EnemyInRange[i].transform.position).sqrMagnitude;
-                if ((pos - EnemyInRange[i].transform.position).sqrMagnitude >= minDistance) continue;
+                var sqrMag = (pos - enemyInRange[i].transform.position).sqrMagnitude;
+                if ((pos - enemyInRange[i].transform.position).sqrMagnitude >= minDistance) continue;
 
                 minDistance = sqrMag;
-                proxTeki = EnemyInRange[i];
+                proxTeki = enemyInRange[i];
             }
 
             ShootBullet(proxTeki);
@@ -103,7 +104,7 @@ namespace MRD
 
         private IEnumerator EnableShooting()
         {
-            yield return new WaitForSeconds(1 / (TowerStat.FinalAttackSpeed * RoundManager.playSpeed));
+            yield return new WaitForSeconds(1 / (TowerStat.FinalAttackSpeed * roundManager.playSpeed));
             onCoolDown = false;
         }
 
@@ -111,16 +112,16 @@ namespace MRD
         {
             if (!col.gameObject.CompareTag("Enemy")) return;
 
-            if(!EnemyInRange.Contains(col.gameObject)) // Probably unneccesary, but just in case
-                EnemyInRange.Add(col.gameObject);
+            if(!enemyInRange.Contains(col.gameObject)) // Probably unnecessary, but just in case
+                enemyInRange.Add(col.gameObject);
         }
 
         private void OnTriggerExit2D(Collider2D col) // Enemy goes out of attack range, remove from watchlist
         {
-            for (int i = 0; i < EnemyInRange.Count; i++)
+            for (int i = 0; i < enemyInRange.Count; i++)
             {
-                if (col.gameObject == EnemyInRange[i])
-                    EnemyInRange.RemoveAt(i);
+                if (col.gameObject == enemyInRange[i])
+                    enemyInRange.RemoveAt(i);
             }
         }
 
