350c58e90280a617d4ec42b7390b5e77c50fba4a
DIA24 <5084kkm@naver.com>
2022-07-18 14:49:06 +0900

    fixed EnemySpawner

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 namespace MRD
 {
     public class EnemySpawner : MonoBehaviour
@@ -8,98 +10,41 @@ namespace MRD
         public GameObject Enemy;
         public GameObject EnemyTemp;
         EnemyType SpawnEnemyType;
-        public float MinX, MaxX, SpawnX; //Enemy스폰 위치 최소 최대 랜덤값
+        public float MinX, MaxX; //Enemy스폰 위치 최소 최대 랜덤값
+        private int NextRoundCheck = 0, WaveMaxSpawn = 0;
         private RoundManager RoundManager;
+        List<GameObject> EnemyList = new List<GameObject>();
 
         public void Start()
         {
             RoundManager = RoundManager.Inst;
         }
-        public void EnemySet(int MaxSpawn,float SpawnTime,EnemyType SpawnEnemyType)
+        public void Update()
         {
-            switch (SpawnEnemyType)
+            if(NextRoundCheck != 0 && NextRoundCheck == WaveMaxSpawn)
             {
-                case EnemyType.E100:
-                    StartCoroutine(SpawnEnemyE100(MaxSpawn, SpawnTime));
-                    break; 
-                case EnemyType.E500:
-                    StartCoroutine(SpawnEnemyE500(MaxSpawn, SpawnTime));
-                    break;
-                case EnemyType.E1000:
-                    StartCoroutine(SpawnEnemyE1000(MaxSpawn, SpawnTime));
-                    break;
-                case EnemyType.E5000:
-                    StartCoroutine(SpawnEnemyE5000(MaxSpawn, SpawnTime));
-                    break;
-                case EnemyType.E10000:
-                    StartCoroutine(SpawnEnemyE10000(MaxSpawn, SpawnTime));
-                    break;
+                if (!EnemyList.Any())
+                {
+                    //NextRound 함수 호출
+                }
+
             }
+                
         }
-        IEnumerator SpawnEnemyE100(int MaxSpawn,float SpawnTime)
+        public void EnemySet(int TypeMaxSpawn,float SpawnTime,EnemyType SpawnEnemyType)
         {
+            StartCoroutine(SpawnEnemy(TypeMaxSpawn, SpawnTime, SpawnEnemyType));
+        }
+        IEnumerator SpawnEnemy(int TypeMaxSpawn, float SpawnTime, EnemyType SpawnEnemyType)
+        {
+            float SpawnX = 0;
             int SpawnCount = 0;
-            while (MaxSpawn > SpawnCount)
+            initEnemyInfo = new EnemyInfo(SpawnEnemyType, 100, 0.1f); //EnemyType, 체력, 속도 (추후 조정)
+
+            while (TypeMaxSpawn > SpawnCount)
             {
                 yield return new WaitForSeconds(SpawnTime);
                 SpawnX = Random.Range(MinX, MaxX);
-                initEnemyInfo = new EnemyInfo(EnemyType.E100, 100, 0.1f); //EnemyType, 체력, 속도 (추후 조정)
-                var newEnemy = Instantiate(Enemy, new Vector3(SpawnX, 17f, 0f), Quaternion.identity);
-                newEnemy.GetComponent<EnemyController>().InitEnemy(initEnemyInfo);
-                SpawnCount++;
-                //RoundManager.OnEnemyCreate(newEnemy);
-            }
-        }
-        IEnumerator SpawnEnemyE500(int MaxSpawn,float SpawnTime)
-        {
-            int SpawnCount = 0;
-            while (MaxSpawn > SpawnCount)
-            {
-                yield return new WaitForSeconds(SpawnTime);
-                SpawnX = Random.Range(MinX, MaxX);
-                initEnemyInfo = new EnemyInfo(EnemyType.E500, 100, 0.1f); //EnemyType, 체력, 속도 (추후 조정)
-                var newEnemy = Instantiate(Enemy, new Vector3(SpawnX, 17f, 0f), Quaternion.identity);
-                newEnemy.GetComponent<EnemyController>().InitEnemy(initEnemyInfo);
-                SpawnCount++;
-                //RoundManager.OnEnemyCreate(newEnemy);
-            }
-        }
-        IEnumerator SpawnEnemyE1000(int MaxSpawn, float SpawnTime)
-        {
-            int SpawnCount = 0;
-            while (MaxSpawn > SpawnCount)
-            {
-                yield return new WaitForSeconds(SpawnTime);
-                SpawnX = Random.Range(MinX, MaxX);
-                initEnemyInfo = new EnemyInfo(EnemyType.E1000, 100, 0.1f); //EnemyType, 체력, 속도 (추후 조정)
-                var newEnemy = Instantiate(Enemy, new Vector3(SpawnX, 17f, 0f), Quaternion.identity);
-                newEnemy.GetComponent<EnemyController>().InitEnemy(initEnemyInfo);
-                SpawnCount++;
-                //RoundManager.OnEnemyCreate(newEnemy);
-            }
-        }
-        IEnumerator SpawnEnemyE5000(int MaxSpawn, float SpawnTime)
-        {
-            int SpawnCount = 0;
-            while (MaxSpawn > SpawnCount)
-            {
-                yield return new WaitForSeconds(SpawnTime);
-                SpawnX = Random.Range(MinX, MaxX);
-                initEnemyInfo = new EnemyInfo(EnemyType.E5000, 100, 0.1f); //EnemyType, 체력, 속도 (추후 조정)
-                var newEnemy = Instantiate(Enemy, new Vector3(SpawnX, 17f, 0f), Quaternion.identity);
-                newEnemy.GetComponent<EnemyController>().InitEnemy(initEnemyInfo);
-                SpawnCount++;
-                //RoundManager.OnEnemyCreate(newEnemy);
-            }
-        }
-        IEnumerator SpawnEnemyE10000(int MaxSpawn, float SpawnTime)
-        {
-            int SpawnCount = 0;
-            while (MaxSpawn > SpawnCount)
-            {
-                yield return new WaitForSeconds(SpawnTime);
-                SpawnX = Random.Range(MinX, MaxX);
-                initEnemyInfo = new EnemyInfo(EnemyType.E10000, 100, 0.1f); //EnemyType, 체력, 속도 (추후 조정)
                 var newEnemy = Instantiate(Enemy, new Vector3(SpawnX, 17f, 0f), Quaternion.identity);
                 newEnemy.GetComponent<EnemyController>().InitEnemy(initEnemyInfo);
                 SpawnCount++;
