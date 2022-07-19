800c3bd8ff44eecdb07b532fd38b9ad86cea1a99
RyuJiSeok <roadmageb@naver.com>
2022-07-19 11:51:49 +0900

    Fix sosamwon checker

--- Assets/Scripts/Yaku/SoSamWon.cs
+++ Assets/Scripts/Yaku/SoSamWon.cs
@@ -9,9 +9,7 @@ namespace MRD
 
         public bool CheckCondition(YakuHolderInfo holder)
         {
-            //조건: 삼원 3개 중 2개로 몸통(깡쯔 혹은 커쯔), 하나로 머리(또이쯔)
-            return holder.MentsuInfos.Count(x => (x is KoutsuInfo or KantsuInfo) && x.Hais[0].Spec.HaiType == HaiType.Sangen) == 2 && // 몸통 2개
-                   holder.MentsuInfos.Count(x => (x is ToitsuInfo) && x.Hais[0].Spec.HaiType == HaiType.Sangen) == 1; //머리 하나
+            return holder.MentsuInfos.Count(x => x.Hais[0].Spec.HaiType == HaiType.Sangen) == 3 && holder.MentsuInfos.Count(x => x is ToitsuInfo) == 1;
         }
     }
 
