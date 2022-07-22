6ea39d73ec24de72ea3fbdc19d70e65eaa3d8888
RyuJiSeok <roadmageb@naver.com>
2022-07-22 20:26:46 +0900

    Fix CheongIl

--- Assets/Scripts/Yaku/CheongIlSaek.cs
+++ Assets/Scripts/Yaku/CheongIlSaek.cs
@@ -8,10 +8,9 @@ namespace MRD
         public string[] OptionNames => new string[] { nameof(CheongIlSaekImageOption), nameof(CheongIlSaekStatOption) };
 
         public bool CheckCondition(YakuHolderInfo holder)
-        {   //청일색: 수패가 모두 같은 모양
-            HaiType cmpbase = holder.MentsuInfos[0].Hais[0].Spec.HaiType; // 비교 기준으로 첫 멘쯔의 모양 설정
-            if (cmpbase is not HaiType.Wan or HaiType.Pin or HaiType.Sou) return false; // 수패가 아니면 false 리턴
-            return holder.MentsuInfos.All(x => (x is KoutsuInfo or KantsuInfo) && x.Hais[0].Spec.HaiType == cmpbase);// 모두 비교대상과 같은지 체크
+        {   
+            var group = holder.MentsuInfos.GroupBy(x => x.Hais[0].Spec.HaiType);
+            return group.Count() == 1 && group.First().Key is HaiType.Wan or HaiType.Pin or HaiType.Sou;
         }
     }
 }
