e0104d5543de7dcf369b9d362bada0488094dd57
Yoon <cse21jh@korea.ac.kr>
2022-08-02 22:01:57 +0900

    Set Yaku Instruction Temp 2

--- Assets/Scripts/YakuInstruction.cs
+++ Assets/Scripts/YakuInstruction.cs
@@ -12,6 +12,8 @@ namespace MRD
         private GameObject instruction;
         private GameObject towerImage;
 
+        private GameObject set;
+
         public Text YakuName;
         public Text YakuCondition;
 
@@ -32,6 +34,13 @@ namespace MRD
         {
             //set 복제
             //각각의 set의 text, image에 역 이름, 역 설명, 타워 이미지 넣기
+
+            //for, 전체 역 개수만큼
+            Instantiate(set, transform.position, Quaternion.identity);
+
+            YakuName.text = "Name";
+            YakuCondition.text = "Condition";
+            //towerImage = ResourceDictionary.Get<GameObject>();
         }
     }
 }
