3cd930e169894dc2b8e6f58128e5cf14f07f59bc
RyuJiSeok <roadmageb@naver.com>
2022-08-03 23:29:20 +0900

    Fix ilgi

--- Assets/Scripts/Yaku/IlGiTongGwan.cs
+++ Assets/Scripts/Yaku/IlGiTongGwan.cs
@@ -11,32 +11,12 @@ namespace MRD
 
         public bool CheckCondition(YakuHolderInfo holder)
         {
-            int hnum = 0;
-            bool[] check = new bool[10];
-            var htype = holder.MentsuInfos[0].Hais[0].Spec.HaiType;
-
-            for (int i = 1; i < 10; i++) check[i] = false;
-            check[0] = true;
-
-            if (holder.MentsuInfos.Any(x => x is ShuntsuInfo))
-                foreach (var p in holder.MentsuInfos.SelectMany(x => x.Hais))
-                {
-                    hnum = p.Spec.Number;
-
-                    if (htype != p.Spec.HaiType) return false;
-
-                    if (!check[hnum]) check[hnum] = true; //check 1~9
-                    htype = p.Spec.HaiType;
-                }
-            //else if(holder.MentsuInfos.Any(x => x is KoutsuInfo or KantsuInfo)) return false; //not Shuntsu
-
-            foreach (bool t in check)
-            {
-                if (!t)
-                    return false;
-            }
-
-            return true;
+            int mask = 0;
+            foreach (var shift in holder.MentsuInfos
+                .Where(x => x is ShuntsuInfo sh && sh.MinNumber % 3 == 1)
+                .Cast<ShuntsuInfo>()
+                .Select(x => 3 * ((int)x.HaiType / 10 - 1) + x.MinNumber / 3)) mask |= 1 << shift;
+            return new int[] { 07, 070, 0700 }.Any(x => (mask & x) == x);
         }
     }
 }
