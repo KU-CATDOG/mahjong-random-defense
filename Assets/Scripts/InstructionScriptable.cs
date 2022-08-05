5fbdfed33230b8204268e2546073b6c7cc9ee682
Yoon <cse21jh@korea.ac.kr>
2022-08-05 15:04:22 +0900

    Add instruction scriptable object (1)

---
+++ Assets/Scripts/InstructionScriptable.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Yaku Instruction", menuName = "ScriptableObjects/Yaku Instruction", order = 1)]
+public class InstructionScriptable : ScriptableObject
+{
+    public string Name;
+    public string Condition;
+
+    public SpriteList[] Image;
+}
+[Serializable]
+public class SpriteList
+{
+    public Sprite[] sprite;
+    public enum GroundType { Triple, Complete, Yakuman }
+    public GroundType groundtype;
+}
