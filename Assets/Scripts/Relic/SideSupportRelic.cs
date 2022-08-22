4d9aa283cb33bfa941d2abd66da3d1204f9b93bf
devingryu <devingryu@korea.ac.kr>
2022-08-22 22:38:09 +0900

    SideSupport

--- Assets/Scripts/Relic/SideSupportRelic.cs
+++ Assets/Scripts/Relic/SideSupportRelic.cs
@@ -5,6 +5,6 @@ namespace MRD
         public override string Name => "SideSupport";
         public override int MaxAmount => 3;
         public override RelicRank Rank => RelicRank.A;
-        public override Stat AdditionalStat(TowerStat towerStat) => new(attackSpeed: towerStat.AttachedTower.Coordinate.Y is 1 or 5?1.1f:1f);
+        public override Stat AdditionalStat(TowerStat towerStat) => new(attackSpeed: towerStat.AttachedTower.Coordinate.Y is 0 or 4?1.1f:1f);
     }
 }
