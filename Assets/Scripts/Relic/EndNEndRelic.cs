d45349afde48485901efe89dd794bff40ef0ee67
RyuJiSeok <roadmageb@naver.com>
2022-08-22 21:21:22 +0900

    Patch EndNEnd

--- Assets/Scripts/Relic/EndNEndRelic.cs
+++ Assets/Scripts/Relic/EndNEndRelic.cs
@@ -4,9 +4,9 @@ namespace MRD
     public class EndNEndRelic : Relic
     {
         public override string Name => "EndNEnd";
-        public override int MaxAmount => 10;
+        public override int MaxAmount => 6;
         public override RelicRank Rank => RelicRank.C;
-        public override Stat AdditionalStat(TowerStat towerStat) => new(critChance: towerStat.TowerInfo.Hais.Count(x => x.Spec.IsRoutou) * 0.01f);
+        public override Stat AdditionalStat(TowerStat towerStat) => new(critChance: towerStat.TowerInfo.Hais.Count(x => x.Spec.IsRoutou) * 0.005f);
         
     }
 }
