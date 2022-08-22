b004874801ea16f044506885de2e507b7bb4ae92
devingryu <devingryu@korea.ac.kr>
2022-08-22 15:50:23 +0900

    ShuKantSu Patch

--- Assets/Scripts/Options/YakuOption/ShuKantSuOption.cs
+++ Assets/Scripts/Options/YakuOption/ShuKantSuOption.cs
@@ -23,18 +23,27 @@ namespace MRD
         {
             // TODO: 도라 랭크업
             // 탄환 삭제. +-40도 내에 50% 느린 추가탄환 8개. 앞으로의 도라가 랭크업 됨(무작위 B랭크 도라, 2개시 무작위 A랭크 도라)
-            if (infos[0] is not BulletInfo info) return;
-            int hornCount = RoundManager.Inst.RelicManager[typeof(HornRelic)];
-            infos.RemoveAt(0);
-
-            for (int i = 0; i < 8; i++)
+            if (infos[0] is BulletInfo info) 
             {
-                float angle = UnityEngine.Random.Range(-40f,40f);
-                infos.Add(new BulletInfo(MathHelper.RotateVector(info.Direction, angle), info.SpeedMultiplier / 2f,
-                    info.ShooterTowerStat, info.StartPosition, info.ImageName, info.ShootDelay, info.Damage));
-                for(int j=0;i<hornCount;i++) 
-                    infos.Add(new BulletInfo(MathHelper.RotateVector(info.Direction, angle), info.SpeedMultiplier / 2f * 0.8f - 0.2f*j,
-                    info.ShooterTowerStat, info.StartPosition, info.ImageName, info.ShootDelay, info.Damage));
+                int hornCount = RoundManager.Inst.RelicManager[typeof(HornRelic)];
+                infos.RemoveAt(0);
+
+                for (int i = 0; i < 8; i++)
+                {
+                    float angle = UnityEngine.Random.Range(-40f,40f);
+                    infos.Add(new BulletInfo(MathHelper.RotateVector(info.Direction, angle), info.SpeedMultiplier / 2f,
+                        info.ShooterTowerStat, info.StartPosition, info.ImageName, info.ShootDelay, info.Damage));
+                    for(int j=0;i<hornCount;i++) 
+                        infos.Add(new BulletInfo(MathHelper.RotateVector(info.Direction, angle), info.SpeedMultiplier / 2f * 0.8f - 0.2f*j,
+                        info.ShooterTowerStat, info.StartPosition, info.ImageName, info.ShootDelay, info.Damage));
+                }
+            }
+            else if(infos[0] is BladeInfo bladeInfo)
+            {
+                foreach(var i in infos.Cast<BladeInfo>())
+                {
+                    i.DamageMultiplier *= 4f;
+                }
             }
         }
     }
