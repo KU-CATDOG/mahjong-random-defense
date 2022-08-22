62c1658fccc137eb56c228cbae6ae5eb64322962
Yoon <cse21jh@korea.ac.kr>
2022-08-22 19:24:30 +0900

    Add tutorial scene (temp)

---
+++ Assets/Scripts/Instruction/Tutorial.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace MRD
+{
+    public class Tutorial : MonoBehaviour
+    {
+        private Sprite[] tutorialBackgroundArr;
+        private int num = 0;
+
+        public ClickUI[] ScreenTouch;
+        public GameObject TutorialScreen;
+
+
+        void Start()
+        {
+            tutorialBackgroundArr = ResourceDictionary.GetAll<Sprite>("Background/tutorial");
+            ShowTutorial();
+        }
+
+        private void ShowTutorial()
+        {
+            Debug.Log("Show!");
+            ScreenTouch[0].AddListenerOnly(() =>
+            {
+                Debug.Log("ClickL");
+                if (num > 0) num -= 1;
+                ShowNextBackground(num);
+            });
+            ScreenTouch[1].AddListenerOnly(() =>
+            {
+                Debug.Log("ClickR");
+                if (num < tutorialBackgroundArr.Length - 1) num += 1;
+                ShowNextBackground(num);
+            });
+        }
+
+        private void ShowNextBackground(int n)
+        {
+            TutorialScreen.GetComponent<Image>().sprite = tutorialBackgroundArr[n];
+            Debug.Log("Hello!");
+            ShowTutorial();
+        }
+    }
+}
